de7d52d36368c0e2923785271c4dfdd39b5b050d
LamenLuan <luan.leme.s35@gmail.com>
2022-01-05 13:48:42 -0300

    Adding execption handling in Kabum and Pichau functions

--- scrapper.py
+++ scrapper.py
@@ -7,24 +7,33 @@ kabumUrl = "https://www.kabum.com.br/produto/115318/headset-gamer-hyperx-cloud-c
 pichauUrl = "https://www.pichau.com.br/headset-gamer-hyperx-cloud-core-som-surround-7-1-drivers-53mm-preto-hx-hscc-2-bk-ww"
 terabyteUrl = "https://www.terabyteshop.com.br/produto/18711/headset-gamer-hyperx-cloud-core-surround-71-35mm-ou-usb-black-hx-hscc-2-bkww"
 
-def prepareSoup(url):
-    webpage_response = requests.get(url)
-    return BeautifulSoup(webpage_response.text, "html.parser")
+def printError(err, function):
+    print("Error: {0} in ".format(err) + function)
 
+def prepareSoup(url):
+    page = requests.get(url)
+    try: return BeautifulSoup(page.text, "html.parser")
+    except AttributeError as err: printError(err, "prepareSoup")
+   
 def prepareSeleniumSoup(driver):
     page = driver.page_source
-    return BeautifulSoup(page, "html.parser")
+    try: return BeautifulSoup(page, "html.parser")
+    except AttributeError as err: printError(err, "prepareSeleniumSoup")
 
 def pichauPrice():
     soup = prepareSoup(pichauUrl)
-    regFinder = re.compile("R\$<!-- -->([0-9]+(.[0-9]+)*,[0-9]{2})")
-    mainDiv = soup.find("div", {"class": "MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-5"})
-    return regFinder.findall(str(mainDiv))[0][0]
+    try:
+        mainDiv = soup.find("div", {"class": "MuiGrid-root MuiGrid-item MuiGrid-grid-xs-12 MuiGrid-grid-sm-5"})
+        regFinder = re.compile("R\$<!-- -->([0-9]+(.[0-9]+)*,[0-9]{2})")
+        return regFinder.findall(str(mainDiv))[0][0]
+    except AttributeError as err: printError(err, "pichauPrice")
 
 def kabumPrice():
     soup = prepareSoup(kabumUrl)
-    priceTag = soup.find("h4", itemprop="price")
-    return priceTag.text.replace("R$\xa0", "")
+    try:
+        priceTag = soup.find("h4", itemprop="price")
+        return priceTag.text.replace("R$\xa0", "")
+    except AttributeError as err: printError(err, "kabumPrice")
 
 def terabytePrice():
     options = webdriver.FirefoxOptions()
@@ -36,6 +45,6 @@ def terabytePrice():
     priceTag = soup.find("p", id="valVista")
     return priceTag.text.replace("R$ ", "")
 
-print( pichauPrice() )
-print( kabumPrice() )
-print( terabytePrice() )
+print( "Kabum: R$ " + kabumPrice() )
+print( "Pichau: R$ " + pichauPrice() )
+print( "Terabyte: R$ " + terabytePrice() )
