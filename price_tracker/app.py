a66f870f73369ec2b9ad36fabddaa51ac28ab57a
LamenLuan <luan.leme.s35@gmail.com>
2022-01-12 15:43:56 -0300

    Minimizing executable when started

--- price_tracker/app.py
+++ price_tracker/app.py
@@ -3,9 +3,16 @@ from google.auth import exceptions
 from tracker.scrapper import scrapper, getBestPrice
 from tracker.misc import printError
 from multiprocessing import freeze_support
+import ctypes
+
+def minimizeWindow():
+    ctypes.windll.user32.ShowWindow(
+        ctypes.windll.kernel32.GetConsoleWindow(), 0
+    )
 
 def runScrapper():
     try:
+        minimizeWindow()
         locale.setlocale(locale.LC_ALL, '')
         print(f"Execution time: {round(scrapper(), 2)}s")
     except TypeError as err:
